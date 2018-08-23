e60da0a5a5850df4857295264c08e96e9bd85c79
Yaroslav Cherednyk <y@devurai.com>
2018-08-23 14:48:40 +0300

    add delete and complete task

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from "../../models/Todo";
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+    selector: 'app-todo',
+    templateUrl: './todo.component.html',
+    styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+    todos: Todo[];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.todos =[
+            {
+                title: 'Task 1',
+                text: 'My text 1',
+                complete: true,
+            },
+            {
+                title: 'Task 2',
+                text: 'My text 2',
+                complete: false,
+            },
+        ]
+    }
+
+    deleteTask(i){
+        this.todos.splice(i, 1);
+    }
+
+    changeTask(i){
+        this.todos[i].complete = !this.todos[i].complete;
+    }
 
 }
