a6fd5ccf57dd084b7155d637185a31b83b85c391
Yaroslav Cherednyk <y@devurai.com>
2018-08-27 16:56:45 +0300

    update delete and add ngif empty

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -14,11 +14,13 @@ export class TodoComponent implements OnInit {
     ngOnInit() {
         this.todos =[
             {
+                id: '1',
                 title: 'Task 1',
                 text: 'My text 1',
                 complete: true,
             },
             {
+                id: '2',
                 title: 'Task 2',
                 text: 'My text 2',
                 complete: false,
@@ -26,12 +28,15 @@ export class TodoComponent implements OnInit {
         ]
     }
 
-    deleteTask(i){
-        this.todos.splice(i, 1);
+    deleteTask(id: string){
+        this.todos = this.todos.filter(task => task.id !== id);
     }
 
     changeTask(i){
         this.todos[i].complete = !this.todos[i].complete;
+    }
+    identify(index){
+        return index;
     }
 
 }
--- src/app/models/Todo.ts
+++ src/app/models/Todo.ts
@@ -1,5 +1,6 @@
 export interface Todo{
     title: string;
     text: string;
-    complete: boolean
+    complete: boolean;
+    id: string;
 }
