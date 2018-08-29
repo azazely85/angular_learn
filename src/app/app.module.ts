0a1ada8f013521969fa51b8052d5173232847064
Yaroslav Cherednyk <y@devurai.com>
2018-08-29 10:47:29 +0300

    create modules task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo/todo.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    HomeComponent,
+    FormComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
