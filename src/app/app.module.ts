cf3f13bb520ed8e1166c731f418b582c3df28a0d
Cherednyk Yaroslav <azazely85@gmail.com>
2018-10-10 16:10:53 +0300

    clear last data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { FormComponent } from './components/form/form.component';
-import { ListComponent } from './components/list/list.component';
-import { ListItemComponent } from './components/list-item/list-item.component';
 import { JsonplaceholderService } from './services/jsonplaceholder.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FlashMessagesModule } from 'angular2-flash-messages';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { PanelComponent } from './components/panel/panel.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
 import { EditBookComponent } from './components/edit-book/edit-book.component';
@@ -20,11 +15,6 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    FormComponent,
-    ListComponent,
-    ListItemComponent,
-    NavbarComponent,
     PanelComponent,
     AddBookComponent,
     EditBookComponent,
