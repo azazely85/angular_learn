d9181fafaaad83d086188ea1497ce919bde78672
Cherednyk Yaroslav <azazely85@gmail.com>
2018-10-10 16:34:19 +0300

    add routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes } from '@angular/router';
+import { PanelComponent } from '../components/panel/panel.component';
+import { AboutComponent } from '../components/about/about.component';
+import { AddBookComponent } from '../components/add-book/add-book.component';
+import { EditBookComponent } from '../components/edit-book/edit-book.component';
+import { NotFoundComponent } from '../components/not-found/not-found.component';
+
+const routes: Routes = [
+    { path: 'panel', component: PanelComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'add-book', component: AddBookComponent},
+    { path: 'books/:id', component: EditBookComponent},
+    { path: '**', component: NotFoundComponent}
+];
+
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    exports: [RouterModule],
+    imports: [
+        RouterModule.forRoot(routes)
+    ]
 })
 export class AppRoutingModule { }
