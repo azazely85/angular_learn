efca119cb5c2c88cacbc5ff3b057b2692fee3575
Cherednyk Yaroslav <azazely85@gmail.com>
2018-10-10 16:10:02 +0300

    create new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { JsonplaceholderService } from './services/jsonplaceholder.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PanelComponent } from './components/panel/panel.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,13 +24,19 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     FormComponent,
     ListComponent,
     ListItemComponent,
-    NavbarComponent
+    NavbarComponent,
+    PanelComponent,
+    AddBookComponent,
+    EditBookComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    FlashMessagesModule.forRoot()
+    FlashMessagesModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [JsonplaceholderService],
   bootstrap: [AppComponent]
