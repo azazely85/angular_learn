d86f209c5487d926dd2de0b94e9b075c7a01c601
Cherednyk Yaroslav <azazely85@gmail.com>
2018-11-01 17:33:04 +0200

    connect login to firebase

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  email: string;
+  password: string;
+  constructor(
+      private authService: AuthService,
+      private router: Router
+  ) { }
 
   ngOnInit() {
   }
+  onSubmit() {
 
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+      private afAuth: AngularFireAuth
+  ) { }
+
+  login(email: string, passord: string) {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.signInWithEmailAndPassword(email, passord).then(user => resolve(user))
+          .catch( error => reject(error));
+    });
+  }
 }
