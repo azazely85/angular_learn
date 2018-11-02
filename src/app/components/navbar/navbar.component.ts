330bf0de02d2ac715c30a70ee67c032024c7ec45
Cherednyk Yaroslav <azazely85@gmail.com>
2018-11-02 16:01:38 +0200

    add redirect after logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -12,6 +13,7 @@ export class NavbarComponent implements OnInit {
 
   constructor(
       private authService: AuthService,
+      private router: Router
   ) { }
 
   ngOnInit() {
@@ -23,7 +25,10 @@ export class NavbarComponent implements OnInit {
       });
   }
   logOut() {
-      this.authService.logOut();
+      this.authService.logOut().then(() => {
+          this.isLogin = false;
+          this.router.navigate(['/login']);
+      });
   }
 
 }
