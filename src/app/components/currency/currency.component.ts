19974a58dbf705613338b5fb8df5a29e9ee8aedb
Cherednyk Yaroslav <azazely85@gmail.com>
2018-11-09 18:04:36 +0200

    add select currency

--- src/app/components/currency/currency.component.ts
+++ src/app/components/currency/currency.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from '../../services/currency.service';
+import { Currency } from '../../models/Currency';
 
 @Component({
   selector: 'app-currency',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./currency.component.css']
 })
 export class CurrencyComponent implements OnInit {
+  selectedCurrency: Currency;
+  currencyList: Currency[];
 
-  constructor() { }
+  constructor(
+      protected currencyService: CurrencyService
+  ) { }
 
   ngOnInit() {
+    this.currencyService.selectedCurrency.subscribe( data => {
+      this.currencyList = data.slice();
+      this.selectedCurrency = Object.create(data.find(obj => obj.isActive));
+    });
+  }
+
+  updateCurrency() {
+    this.currencyService.selectCurrency(this.selectedCurrency.name);
   }
 
 }
--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Currency } from '../models/Currency';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
+  currency: Currency[] = [
+      {
+        name: 'USD',
+        isActive: true,
+        coefficient: 1
+      },
+      {
+        name: 'EUR',
+        isActive: false,
+        coefficient: 0.5
+      },
+      {
+        name: 'GBP',
+        isActive: false,
+        coefficient: 0.9
+      }
+  ];
+  private currencySourse = new BehaviorSubject<Currency[]>(this.currency);
+  selectedCurrency = this.currencySourse.asObservable();
 
   constructor() { }
+
+  selectCurrency(name: string) {
+
+  }
 }
